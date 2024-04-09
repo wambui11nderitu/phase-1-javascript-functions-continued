62bc3704fdb81dd44dfd625348de9b89ac67ae41
Wambui <nderituwambui93@gmail.com>
2024-04-09 13:50:03 +0300

    functions continued

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+// Function saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Function mondayWork
+  function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+  }
+  
+  // Function wrapAdjective
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+  // Example usage of wrapAdjective
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("amazing")); // Output: You are !!!amazing!!!
+  
